b4f83cc1f2fdc4105d3a74cd6bf965f1cae42c18
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-15 22:54:04 +0530

    0704-binary-search Array .py Easy

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif target<nums[mid]:
+                r-=1
+            else:
+                l+=1
+        return -1
+        
