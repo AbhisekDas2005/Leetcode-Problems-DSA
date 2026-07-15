c05bb3813df6b1567820ca646cdc68f45beee560
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-15 18:25:17 +0530

    0496-next-greater-element-i Array .py Easy

---
+++ LeetCode/Easy/0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l=[-1]*len(nums1)
+        for i in range(len(nums1)):
+            n=nums2.index(nums1[i])
+            for j in range(n,len(nums2)):
+                if nums2[j]>nums1[i]:
+                    l[i]=nums2[j]
+                    break
+        return l
+
+
+        
