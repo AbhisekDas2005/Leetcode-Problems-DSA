a3d81361facc27d686318b9759afca0b400bd9b9
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-16 11:24:09 +0530

    2011-final-value-of-variable-after-performing-operations Array .java Easy

---
+++ LeetCode/Easy/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int s=0;
+        for(int i=0;i<operations.length;i++){
+            if (operations[i].charAt(1)=='+'){
+                s+=1;
+            }else{
+                s-=1;
+            }
+        }
+        return s;
+    }
+}
