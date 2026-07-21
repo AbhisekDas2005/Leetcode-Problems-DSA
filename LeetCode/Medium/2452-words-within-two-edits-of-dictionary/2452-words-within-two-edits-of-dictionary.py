c47004d27384be3eeedbb701a2799e9a91736e26
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-21 19:29:38 +0530

    2452-words-within-two-edits-of-dictionary Array .py Medium

---
+++ LeetCode/Medium/2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        a=[]
+        for i in queries:
+            for j in dictionary:
+                d=0
+                for k in range(len(i)):
+                    if i[k]!=j[k]:
+                        d+=1
+                        if d>2:
+                            break
+                if d<=2:
+                    a.append(i)
+                    break
+        return a
+        
