1d4d217c27a95b8c0d751c38878fc12550d25da3
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-23 11:02:38 +0530

    2442-count-number-of-distinct-integers-after-reverse-operations Array .py Medium

--- LeetCode/Medium/2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ LeetCode/Medium/2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -8,19 +8,16 @@ class Solution:
                 l.append(int(str(i)[::-1]))
         nums.extend(l)
         return(len(set(nums)))
-        # c=0
-        # s=[]
+
+        #solution 2(attempted??)
+        # c = 0
+        # s = []
         # for i in nums:
-        #     if len(str(i))==1 and str(i) not in s:
-        #         c+=1
-        #         s.append(int(str(i)))
-        #     else:
-        #         if str(i)==str(i)[::-1] and int(str(i)) not in s:
-        #             c+=1
-        #         else:
-        #             if int(str(i)) not in s:
-        #                 c+=1
-        #             if int(str(i)[::-1]) not in s:
-        #                 c+=1
+        #     rev = int(str(i)[::-1])
+        #     if i not in s:
+        #         s.append(i)
+        #         c += 1
+        #     if rev not in s:
+        #         s.append(rev)
+        #         c += 1
         # return c
-        
