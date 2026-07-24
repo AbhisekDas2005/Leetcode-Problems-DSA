3cb430275e9f87640956e2ae28cfe92b8b7fa33b
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-24 21:17:40 +0530

    3918-sum-of-primes-between-number-and-its-reverse Math .py Medium

--- LeetCode/Medium/3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.py
+++ LeetCode/Medium/3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.py
@@ -1,17 +1,14 @@
 class Solution:
     def sumOfPrimesInRange(self, n: int) -> int:
         s=0
-        for i in range(min(n,int(str(n)[::-1])),max(n,int(str(n)[::-1]))+1):
-            if i==1:
+        for i in range(min(n, int(str(n)[::-1])), max(n, int(str(n)[::-1])) + 1):
+            if i < 2:
                 continue
-            if i==2:
-                s+=2
-                continue
-            for j in range(2,i//2+1):
-                if i%j==0:
+
+            for j in range(2, int(i**0.5) + 1):
+                if i % j == 0:
                     break
             else:
-                print(i)
-                s+=i
+                s += i
         return s
         
