4a05dc73bca058de91afa04bb13d7c8e9b48b3fc
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-07-28 11:06:36 +0530

    3527-find-the-most-common-response Array .py Medium

---
+++ LeetCode/Medium/3527-find-the-most-common-response/3527-find-the-most-common-response.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCommonResponse(self, responses: List[List[str]]) -> str:
+        d={}
+        a=[]
+        for i in responses:
+            a.append(set(i))
+        for i in a:
+            for j in i:
+                if j not in d:
+                    d[j]=1
+                else:
+                    d[j]+=1
+        d = dict(sorted(d.items(), key=lambda item: (-item[1], item[0])))
+        return(next(iter(d)))
+
+            
+                    
+            
+            
