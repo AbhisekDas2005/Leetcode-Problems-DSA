03ab4b15fbc91abdec716cf5fa865cfc9ed2a3a3
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-08-02 22:00:55 +0530

    0877-stone-game Array .py Medium

--- LeetCode/Medium/0877-stone-game/0877-stone-game.py
+++ LeetCode/Medium/0877-stone-game/0877-stone-game.py
@@ -2,11 +2,12 @@ from functools import cache
 
 class Solution:
     def stoneGame(self,piles:List[int])->bool:
-        @cache
-        def dp(l,r):
-            if l==r:
-                return piles[l]
-            take_left=piles[l]-dp(l+1,r)
-            take_right=piles[r]-dp(l,r-1)
-            return max(take_left,take_right)
-        return dp(0,len(piles)-1)>0
+        # @cache
+        # def dp(l,r):
+        #     if l==r:
+        #         return piles[l]
+        #     take_left=piles[l]-dp(l+1,r)
+        #     take_right=piles[r]-dp(l,r-1)
+        #     return max(take_left,take_right)
+        # return dp(0,len(piles)-1)>0
+        return True
