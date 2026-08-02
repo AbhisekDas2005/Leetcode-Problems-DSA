a9c53480ac5e6b2f8f2e9e31b571bfaddbdcd6b6
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-08-03 02:20:03 +0530

    0733-flood-fill Array .py Easy

---
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        visit=set()
+        def dfs(i,j):
+            if i<0 or i>=len(image) or j<0 or j>=len(image[0]):
+                return
+            if (i,j) in visit:
+                return
+            if image[i][j]!=orig:
+                return
+
+            visit.add((i,j))
+            image[i][j]=color
+
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        orig=image[sr][sc]
+        dfs(sr,sc)
+        return image
