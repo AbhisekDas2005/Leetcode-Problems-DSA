5b0930527adc4f1c0e4ef303cec143e6be563a19
AbhisekDas2005 <abhisekdas2005@gmail.com>
2026-08-04 20:38:27 +0530

    3731-find-missing-elements Array .py Easy

---
+++ LeetCode/Easy/3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        mi=min(nums)
+        ma=max(nums)
+        l=[]
+        for i in range(mi+1,ma):
+            if i not in nums:
+                l.append(i)
+        return l
